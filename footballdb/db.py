8cecc768c4ee42f63a9f94bd7c9423c3a43e74a8
Josh <josh@josh-VirtualBox.(none)>
2015-05-11 17:00:30 +1000

    updating git

---
+++ footballdb/db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+###############################################################################
+#                                                                             #
+#    db.py                                                                    #
+#                                                                             #
+#    Main entry point for the footballdb software                             #
+#                                                                             #
+#    Copyright (C) Josh Daly                                                  #
+#                                                                             #
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+__author__ = "Josh Daly"
+__copyright__ = "Copyright 2015"
+__credits__ = ["Josh Daly"]
+__license__ = "GPLv3"
+__version__ = "0.1.0"
+__maintainer__ = "Josh Daly"
+__email__ = "joshua.daly@uqconnect.edu.au"
+__status__ = "Dev"
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
